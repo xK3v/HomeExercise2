bdcc0771b4f1459e5e3fd8f3adc7c261b3eefeb5
xK3v <kazianschuetz.kevin@gmail.com>
2019-01-12 18:17:29 +0100

    pretty much everything

--- app/src/main/java/at/fh/swengb/kazianschuetz/homeexercise2/AddNoteActivity.kt
+++ app/src/main/java/at/fh/swengb/kazianschuetz/homeexercise2/AddNoteActivity.kt
@@ -2,11 +2,29 @@ package at.fh.swengb.kazianschuetz.homeexercise2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_add_note.*
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var myDb: NoteDatabase
+    private lateinit var myAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+
+        myDb = NoteDatabase.getDatabase(applicationContext)
+
+        myAdapter = NoteAdapter()
+
+
+    }
+
+    fun saveNote(v:View) {
+        val newNote = Note(txt_title.text.toString(),txt_content.text.toString())
+        myDb.noteDao.insert(newNote)
+        myAdapter.updateData(myDb.noteDao.findAllNotes())
+        finish()
     }
 }
--- app/src/main/java/at/fh/swengb/kazianschuetz/homeexercise2/NoteListActivity.kt
+++ app/src/main/java/at/fh/swengb/kazianschuetz/homeexercise2/NoteListActivity.kt
@@ -1,24 +1,44 @@
 package at.fh.swengb.kazianschuetz.homeexercise2
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import kotlinx.android.synthetic.main.activity_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
 
-    lateinit var sharedPreferences: SharedPreferences
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var myAdapter: NoteAdapter
+    private lateinit var myDb: NoteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
         sharedPreferences = getSharedPreferences(packageName, Context.MODE_PRIVATE)
-        var name = sharedPreferences.getString("NAME","-")
-        var age = sharedPreferences.getInt("AGE",0)
+        val name = sharedPreferences.getString("NAME", "-")
+        val age = sharedPreferences.getInt("AGE", 0)
 
         lb_head.text = "Notes for $name, $age"
 
+        myDb = NoteDatabase.getDatabase(applicationContext)
 
+        myAdapter = NoteAdapter()
+
+        rv_notes.adapter = myAdapter
+        rv_notes.layoutManager = LinearLayoutManager(this)
+
+
+        myAdapter.updateData(myDb.noteDao.findAllNotes())
     }
+
+    fun addNote(v:View) {
+        val intent = Intent(this, AddNoteActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
